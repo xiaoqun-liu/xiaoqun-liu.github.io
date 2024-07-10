742c077b79746e2b083b7a6253ae7eb4ace77606
xiaoqun-liu <lxqwill@gmail.com>
2024-07-10 21:03:34 +0800

    slides

--- js/slide.js
+++ js/slide.js
@@ -2,23 +2,28 @@ const slides = document.querySelectorAll('.slide');
 let currentSlideIndex = 0;
 
 function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.display = i === index ? 'block' : 'none';
-  });
+    //   slides.forEach((slide, i) => {
+    //     slide.style.display = i === index ? 'block' : 'none';
+    //   });
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+            slide.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        } else {
+            slide.style.display = 'none';
+        }
+    });
 }
 
 function nextSlide() {
-  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-  showSlide(currentSlideIndex);
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
 }
-
-// 每 5 秒自动切换到下一张幻灯片
-// setInterval(nextSlide, 5000);
 
 showSlide(currentSlideIndex);
 
 const slideContainer = document.querySelector('.slide-container');
 slideContainer.addEventListener('click', () => {
-//   clearInterval(autoSlideInterval);
-  nextSlide();
+    //   clearInterval(autoSlideInterval);
+    nextSlide();
 });
